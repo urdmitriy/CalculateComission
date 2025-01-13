7a1794e4880c3e86cf57745ea6842ba6bd13cacc
d.uryupin <d.uryupin@stroytlm.ru>
2025-01-13 21:37:21 +0300

    Задание 1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,8 @@
 fun main(){
-
+    val comissionTax = 0.75
+    val comissionMin = 35
+    val amount = 3_000
+    val commission = amount * comissionTax / 100
+    val totalCommission = if (commission < comissionMin) comissionMin else commission
+    println("Комиссия составит $totalCommission")
 }
